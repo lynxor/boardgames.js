9e7fcce46f8087c174571616f0177b3fca4a6646
daafvader <dawid.malan@gmail.com>
2012-06-29 16:09:20 +0200

    Put back tests

--- tests/gametree_test.js
+++ tests/gametree_test.js
@@ -4,22 +4,22 @@ var b = require("../js/board.js"),
 
 
 describe("The gametree", function () {
-//    it("obvious first move", function () {
-//        var res = g.next(b.CROSS, b.instance(), 2);
-//
-//        assert.deepEqual([1,1], res);
-//    });
-//
-//    it("obvious blocking", function () {
-//        var board = b.instance();
-//        board.put(1,1, b.CROSS);
-//        board.put(0,0, b.CIRCLE);
-//        board.put(1,0, b.CROSS);
-//
-//        var res = g.next(b.CIRCLE, board, 4);
-//
-//        assert.deepEqual([1,2], res);
-//    });
+    it("obvious first move", function () {
+        var res = g.next(b.CROSS, b.instance(), 2);
+
+        assert.deepEqual([1,1], res);
+    });
+
+    it("obvious blocking", function () {
+        var board = b.instance();
+        board.put(1,1, b.CROSS);
+        board.put(0,0, b.CIRCLE);
+        board.put(1,0, b.CROSS);
+
+        var res = g.next(b.CIRCLE, board, 4);
+
+        assert.deepEqual([1,2], res);
+    });
 
     it("obvious winning move", function () {
         var board = b.instance();
